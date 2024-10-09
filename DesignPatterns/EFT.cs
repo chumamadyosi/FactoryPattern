7ecb021f400bb0d16cfe423ce079cb75ad25fec8
Chumam <chumam@hollywoodbets.net>
2024-10-09 14:40:30 +0200

    init

---
+++ DesignPatterns/EFT.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class EFT : IPayment
+    {
+        Task<bool> IPayment.Pay(double amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DesignPatterns/PamentFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    internal class PamentFactory
+    {
+        public static IPayment create(string type)
+        {
+            switch (type) {
+                case "eft":
+                        return new EFT();
+                case "credid":
+                    return new CrediCardPament();
+                default: throw new NotImplementedException();
+            }
+        }
+    }
+}
